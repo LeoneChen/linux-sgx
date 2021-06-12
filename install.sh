a2f1bdb454a431633cf76a85ecfc080fadb7ae8a
leone <791960492@qq.com>
2021-06-12 22:56:53 +0800

    update

--- install.sh
+++ install.sh
@@ -1,32 +1,36 @@
+# pass FLAG
 if [ -n "$1" ]; then
   FLAG=$1
 fi
 
+# target-sdk_install_pkg depends on target-sdk
 make sdk_install_pkg ${FLAG} -s
 if [ $? -ne 0 ]; then
-  echo "error: make sdk_install_pkg"
+  echo "[Error] make sdk_install_pkg"
   exit
 fi
-echo "success: make sdk_install_pkg"
+echo "[Success] make sdk_install_pkg"
 
+# install sgx-sdk
 echo "c" | sudo -S ./linux/installer/bin/sgx_linux_x64_sdk_*.bin <<EOF
 no
 /opt/intel/
 EOF
 if [ $? -ne 0 ]; then
-  echo "error: install sgx sdk"
+  echo "[error] install sgx sdk"
   exit
 fi
-echo "success: install sgx sdk"
+echo "[success] install sgx sdk"
 
+# target-deb_local_repo depends on target-deb_psw_pkg which indirectly depends on target-psw
 make deb_local_repo ${FLAG} -s
 if [ $? -ne 0 ]; then
-  echo "error: make deb_local_repo"
+  echo "[Error] make deb_local_repo"
   exit
 fi
-echo "success: make deb_local_repo"
+echo "[Success] make deb_local_repo"
 
 echo "c" | sudo -S apt update
 echo "c" | sudo -S apt-get install libsgx-launch libsgx-urts libsgx-epid libsgx-quote-ex libsgx-dcap-ql -y
 echo "c" | sudo -S apt-get install libsgx-launch-dbgsym libsgx-urts-dbgsym libsgx-epid-dbgsym libsgx-quote-ex-dbgsym libsgx-dcap-ql-dbgsym -y
-echo "c" | sudo -S apt-get install libsgx-launch-dev libsgx-urts-dev libsgx-epid-dev libsgx-quote-ex-dev libsgx-dcap-ql-dev -y
+# echo "c" | sudo -S apt-get install libsgx-launch-dev libsgx-urts-dev libsgx-epid-dev libsgx-quote-ex-dev libsgx-dcap-ql-dev -y
--- pre_ubuntu_20.04.sh
+++ pre_ubuntu_20.04.sh
@@ -1,14 +1,18 @@
-sudo apt-get install build-essential ocaml ocamlbuild automake autoconf libtool wget python3 python-is-python3 libssl-dev git cmake perl -y
-sudo apt-get install libssl-dev libcurl4-openssl-dev protobuf-compiler libprotobuf-dev debhelper cmake reprepro unzip -y
-sudo apt-get install build-essential python2 -y
-sudo apt-get install libssl-dev libcurl4-openssl-dev libprotobuf-dev -y
+sudo apt-get install build-essential ocaml ocamlbuild automake autoconf libtool wget python2 python-is-python3 libssl-dev git cmake perl libssl-dev libcurl4-openssl-dev protobuf-compiler libprotobuf-dev debhelper cmake reprepro unzip build-essential libssl-dev libcurl4-openssl-dev libprotobuf-dev -y
 
 make preparation
 sudo cp external/toolset/ubuntu20.04/{as,ld,ld.gold,objdump} /usr/local/bin
 
-if [ $(grep -Ec "^[^#].*sgx_debian_local_repo" /etc/apt/sources.list) -eq 0 ]; then
-  echo "add apt source"
-  sudo sh -c "echo \"deb [trusted=yes arch=amd64] file:/home/leone/文档/linux-sgx/linux/installer/deb/sgx_debian_local_repo focal main\" >>/etc/apt/sources.list"
-else
-  echo "already added apt source"
+list_dir=/etc/apt/sources.list.d
+psw_list=$list_dir/intel-sgx-psw.list
+
+if [ ! -d "$list_dir" ]; then
+  mkdir "$list_dir"
 fi
+
+if [ ! -f "$psw_list" ]; then
+  sudo touch "$psw_list"
+  sudo sh -c "echo \"deb [trusted=yes arch=amd64] file:/home/leone/文档/linux-sgx/linux/installer/deb/sgx_debian_local_repo focal main\" >> $psw_list"
+fi
+
+echo "Finish adding apt source"
